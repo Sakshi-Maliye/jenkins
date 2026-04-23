68f6675f05cb5c935adf99cf0f71192c6fc989fe
Sakshi Maliye <sakshimaliye18@gmail.com>
2026-04-23 19:02:12 +0530

    Add files via upload

---
+++ Cal.java
@@ -0,0 +1,16 @@
+package bnmit;
+
+/**
+ * Hello world!
+ */
+public class Cal {
+    public static void main(String[] args) {
+        
+            System.out.println("hello");
+        
+    }
+    public int add(int a, int b) {
+        return a+b;
+        
+    }
+}
---
+++ CalTest.java
@@ -0,0 +1,21 @@
+package bnmit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test() {
+        Cal c=new Cal();
+        int res=c.add(2,2);
+        assertEquals(4,res);
+    }
+}
